43e5879cbad66910c06b5cb1c9c460dc4a9e5d67
Adriano Barbosa <adriano.barbosa100@gmail.com>
2024-02-03 13:21:45 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import { app } from "./app";
+import { env } from "./env";
+
+const start = async () => {
+  try {
+    await app
+      .listen({
+        host: "0.0.0.0",
+        port: env.PORT,
+      })
+      .then(() => {
+        console.log(`Server rodando na PORT ${env.PORT}`);
+      });
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+start();
