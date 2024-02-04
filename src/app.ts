07cfd81df28ed33c8a9ab1ff0c945ba10ce6ad97
Adriano Barbosa <adriano.barbosa100@gmail.com>
2024-02-04 09:30:14 -0300

    feat: add erro handler

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,25 @@
 import fastify from "fastify";
+import { ZodError } from "zod";
+import { env } from "./env";
+import { appRoutes } from "./http/routes/routes";
 
 export const app = fastify();
+
+app.register(appRoutes);
+
+app.setErrorHandler((error, req, res) => {
+  if (error instanceof ZodError) {
+    return res.status(400).send({
+      message: "validation error",
+      issues: error.format(),
+    });
+  }
+
+  if (env.NODE_ENV === "production") {
+    console.error(error);
+  } else {
+    // ferramenta de observalidade
+  }
+
+  return res.status(500).send({ message: "Internal server error." });
+});
