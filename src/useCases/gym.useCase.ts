228e95238bd61c48500448332bf65383be1df2d7
Adriano Barbosa <adriano.barbosa100@gmail.com>
2024-02-05 17:29:13 -0300

    feat: add gym use case

---
+++ src/useCases/gym.useCase.ts
@@ -0,0 +1,38 @@
+import { GymRepository } from "@/repositories/gyms.repository";
+import { Gym } from "@prisma/client";
+
+interface GymUseCaseRequest {
+  title: string;
+  description: string | null;
+  phone: string | null;
+  latitude: number;
+  longitude: number;
+}
+
+interface GymUseCaseResponse {
+  gym: Gym;
+}
+
+export class GymUseCase {
+  constructor(private gymRepository: GymRepository) {}
+
+  async create({
+    title,
+    description,
+    phone,
+    latitude,
+    longitude,
+  }: GymUseCaseRequest): Promise<GymUseCaseResponse> {
+    const gym = await this.gymRepository.create({
+      title,
+      description,
+      phone,
+      latitude,
+      longitude,
+    });
+
+    return {
+      gym,
+    };
+  }
+}
