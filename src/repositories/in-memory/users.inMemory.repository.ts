c3dda9008bbc29507d7c70cc835cc80d11230bc7
Adriano Barbosa <adriano.barbosa100@gmail.com>
2024-02-05 17:27:12 -0300

    feat: add user in memory

--- src/repositories/in-memory/users.inMemory.repository.ts
+++ src/repositories/in-memory/users.inMemory.repository.ts
@@ -1,27 +1,31 @@
 import { Prisma, User } from "@prisma/client";
-import { UsersInterfaceRepository } from "../users.interface.repository";
+import { randomUUID } from "crypto";
+import { UsersRepository } from "../users.repository";
 
-export class UsersInMemoryRepository implements UsersInterfaceRepository {
+export class UsersInMemoryRepository implements UsersRepository {
   public items: User[] = [];
+
+  async findById(id: string) {
+    const user = this.items.find((item) => item.id === id);
+    if (!user) return null;
+    return user;
+  }
 
   async create(data: Prisma.UserCreateInput) {
     const user = {
-      id: "user-1",
+      id: randomUUID(),
       name: data.name,
       email: data.email,
       passwordHash: data.passwordHash,
       createdAt: new Date(),
     };
-
     this.items.push(user);
-
     return user;
   }
 
   async findByEmail(email: string) {
     const user = this.items.find((item) => item.email === email);
     if (!user) return null;
-
     return user;
   }
 }
