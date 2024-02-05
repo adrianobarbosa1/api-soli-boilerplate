61542e220479ea5ad5a421392bb0b4f490e65a93
Adriano Barbosa <adriano.barbosa100@gmail.com>
2024-02-05 17:27:00 -0300

    feat: add gym in memory

---
+++ src/repositories/in-memory/gyms.inMemory.repository.ts
@@ -0,0 +1,31 @@
+import { Gym, Prisma } from "@prisma/client";
+import { randomUUID } from "crypto";
+import { GymRepository } from "../gyms.repository";
+
+export class GymInMemoryRepository implements GymRepository {
+  public items: Gym[] = [];
+
+  async create(data: Prisma.GymCreateInput) {
+    const gym = {
+      id: data.id ?? randomUUID(),
+      title: data.title,
+      description: data.description ?? null,
+      phone: data.phone ?? null,
+      latitude: new Prisma.Decimal(data.latitude.toString()),
+      longitude: new Prisma.Decimal(data.longitude.toString()),
+      createdAt: new Date(),
+    };
+    this.items.push(gym);
+    return gym;
+  }
+
+  async findById(id: string) {
+    const gym = this.items.find((item) => item.id === id);
+    if (!gym) return null;
+    return gym;
+  }
+
+  searchMany(query: string, page: number) {
+    throw new Error("Method not implemented.");
+  }
+}
