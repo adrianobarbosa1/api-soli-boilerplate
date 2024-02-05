96c4cbe182bdefe643ecfdd7fd24bd6c595be391
Adriano Barbosa <adriano.barbosa100@gmail.com>
2024-02-05 17:26:02 -0300

    feat: add gym repository

---
+++ src/repositories/gyms.repository.ts
@@ -0,0 +1,8 @@
+import { Gym, Prisma } from "@prisma/client";
+
+export interface GymRepository {
+  create(data: Prisma.GymCreateInput): Promise<Gym>;
+  findById(id: string): Promise<Gym | null>;
+  // findManyNearby(params: FindManyNearbyParams): Promise<Gym[]>;
+  // searchMany(query: string, page: number): Promise<Gym[]>;
+}
