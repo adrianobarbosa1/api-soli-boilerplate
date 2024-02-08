2343db1a92321ac5a4ad03fcc1ab609cb1929b77
Adriano Barbosa <adriano.barbosa100@gmail.com>
2024-02-08 18:10:54 -0300

    feat: add config vite environment global

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,8 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    environmentMatchGlobs: [["src/test/e2e/**", "prisma"]],
+    dir: "src",
+  },
 });
