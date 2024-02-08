796c4bb0362ed7b58b24ceb01e8f9cc7341bbad1
Adriano Barbosa <adriano.barbosa100@gmail.com>
2024-02-08 18:11:44 -0300

    feat: add logic connect db

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,30 @@
 import { app } from "./app";
+import { prisma } from "./database/prismaClient";
 import { env } from "./env";
+import { startLoadingEffect, stopLoadingEffect } from "./utils/loadingEffect";
 
 const start = async () => {
+  let loadingInterval: NodeJS.Timeout | null = null;
   try {
+    loadingInterval = startLoadingEffect();
+    await prisma.$connect();
+    stopLoadingEffect(loadingInterval);
+    console.log("Connected to database ✅");
+
     await app
       .listen({
         host: "0.0.0.0",
         port: env.PORT,
       })
       .then(() => {
-        console.log(`Server rodando na PORT ${env.PORT}`);
+        console.log(`Listening on port  http://localhost:${env.PORT} ✅`);
       });
   } catch (err) {
-    app.log.error(err);
+    if (loadingInterval !== null) stopLoadingEffect(loadingInterval);
+    console.error("Failed to connect to database ❌");
+    console.error(err);
     process.exit(1);
   }
 };
+
 start();
